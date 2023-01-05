5c2cc2838771c533853107cbe8162a9fad1e67a5
Yonatan Amare <yonatanamare5000@gmail.com>
2023-01-05 15:11:51 +0300

    fix login page

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,48 +21,37 @@
 
     session_start();
 
-
     $_SESSION["user"]="";
     $_SESSION["usertype"]="";
-
-    
     
     // Set the new timezone
-    date_default_timezone_set('Asia/Aden');
+    date_default_timezone_set('Asia/Kolkata');
     $date = date('Y-m-d');
 
     $_SESSION["date"]=$date;
     
 
     //import database
-
     include("connection.php");
 
     
 
- 
 
 
     if($_POST){
 
         $email=$_POST['useremail'];
         $password=$_POST['userpassword'];
-        $password=$_POST ['userpassword'];
         
         $error='<label for="promter" class="form-label"></label>';
 
-        
-                
-      
         $result= $database->query("select * from webuser where email='$email'");
-        
         if($result->num_rows==1){
             $utype=$result->fetch_assoc()['usertype'];
             if ($utype=='p'){
                 //TODO
-                $checker = $database->query("SELECT * FROM `account` where pemail='$email' and ppassword='$password'");
+                $checker = $database->query("select * from patient where pemail='$email' and ppassword='$password'");
                 if ($checker->num_rows==1){
-           
 
 
                     //   Patient dashbord
@@ -72,17 +60,9 @@
                     
                     header('location: patient/index.php');
 
-            
-            if (password_verify($password, $userpassword)){
-                //TODO
-                $checker = $database->query("SELECT * FROM `account` where pemail='$email' and ppassword='$password'");
-                if ($checker->num_rows==1){
-
                 }else{
                     $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Wrong credentials: Invalid email or password</label>';
                 }
-                
-                
 
             }elseif($utype=='a'){
                 //TODO
@@ -90,12 +70,9 @@
                 if ($checker->num_rows==1){
 
 
-
                     //   Admin dashbord
                     $_SESSION['user']=$email;
                     $_SESSION['usertype']='a';
-                   
-                   
                     
                     header('location: admin/index.php');
 
@@ -106,7 +83,7 @@
 
             }elseif($utype=='d'){
                 //TODO
-                $checker = $database->query("SELECT * FROM `doctor` WHERE docemail='$email' AND docpassword='$password'");
+                $checker = $database->query("select * from doctor where docemail='$email' and docpassword='$password'");
                 if ($checker->num_rows==1){
 
 
@@ -124,32 +101,103 @@
         }else{
             $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">We cant found any acount for this email.</label>';
         }
-    }elseif($utype=='d'){
+
+
+
+    }elseif($utype=='dev'){
         //TODO
-        $checker = $database->query("SELECT * FROM `doctor` WHERE docemail='$email' AND docpassword='$password'");
+        $checker = $database->query("select * from developers where devemail='$email' and devpassword='$password'");
         if ($checker->num_rows==1){
-
-
+    
+    
             //   doctor dashbord
             $_SESSION['user']=$email;
-            $_SESSION['usertype']='d';
-            header('location: doctor/index.php');
-
+            $_SESSION['usertype']='dev';
+            header('location: developers/index.php');
+    
         }else{
             $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Wrong credentials: Invalid email or password</label>';
         }
-
+    
     }
     
-}
-    
-
-
-
-
-
-    }else{
-    $error='<label for="promter" class="form-label">&nbsp;</label>';
-}
+        
+    else{
+        $error='<label for="promter" class="form-label">&nbsp;</label>';
+    }
 
     ?>
+
+
+
+
+
+    <center>
+    <div class="container">
+        <table border="0" style="margin: 0;padding: 0;width: 60%;">
+            <tr>
+                <td>
+                    <p class="header-text">Welcome Back!</p>
+                </td>
+            </tr>
+        <div class="form-body">
+            <tr>
+                <td>
+                    <p class="sub-text">Login with your details to continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td">
+                    <label for="useremail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="email" name="useremail" class="input-text" placeholder="Email Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <label for="userpassword" class="form-label">Password: </label>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="label-td">
+                    <input type="Password" name="userpassword" class="input-text" placeholder="Password" required>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td><br>
+                <?php echo $error ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="submit" value="Login" class="login-btn btn-primary btn">
+                </td>
+            </tr>
+        </div>
+            <tr>
+                <td>
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Don't have an account&#63; </label>
+                    <a href="signup.php" class="hover-link1 non-style-link">Sign Up</a>
+                    <br><br><br>
+                </td>
+            </tr>
+                        
+                        
+    
+                        
+                    </form>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
